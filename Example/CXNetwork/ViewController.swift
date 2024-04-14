a427759f00d5ce4ed6431647aa65200ebf412e28
Teng Fei <hansen981@126.com>
2024-04-15 04:48:45 +0800

    add the samples for invoking networking request

--- Example/CXNetwork/ViewController.swift
+++ Example/CXNetwork/ViewController.swift
@@ -8,6 +8,23 @@
 
 import UIKit
 import CXNetwork_Moya
+import ObjectMapper
+
+struct User: Mappable {
+    var name: String?
+    var age: Int = 0
+    var gender: Int = 1
+    
+    init?(map: ObjectMapper.Map) {
+        
+    }
+    
+    mutating func mapping(map: ObjectMapper.Map) {
+        name    <- map["name"]
+        age     <- map["age"]
+        gender  <- map["gender"]
+    }
+}
 
 class ViewController: UIViewController {
     
@@ -19,7 +36,7 @@ class ViewController: UIViewController {
         CXNetWorkManager.shared.request(api: StreamAPI(downloadURL: URL(string: url)!, toDirectory: "Images")) { result in
             switch result {
             case .success(let data):
-                print("[I] filePath: \(String(data: data, encoding: .utf8))")
+                print("[I] filePath: \(String(describing: String(data: data, encoding: .utf8)))")
             case .failure(let error):
                 print("[E] error: \(error)")
             }
@@ -27,12 +44,25 @@ class ViewController: UIViewController {
         
         // Gets an image data.
         let base = "https://xxx.xxx.xx"
-        let imgCodePath = base + "/auth/v1/verify/phoneImgCode"
+        let imgCodePath = "/auth/v1/verify/phoneImgCode"
         CXNetWorkManager.shared.request(api: API(baseUrl: base, path: imgCodePath, method: .get)) { result in
             switch result {
             case .success(let data):
                 let image = UIImage(data: data)
-                print("[I] imageData: \(data), image: \(image)")
+                print("[I] imageData: \(data), image: \(String(describing: image))")
+            case .failure(let error):
+                print("[E] error: \(error)")
+            }
+        }
+        
+        // Gets an image data.
+        let base2 = "https://xxx.xxx.xx"
+        let userPath = "/get/user"
+        ListResponse<User>.request(api: API(baseUrl: base2, path: userPath, method: .get)) { result in
+            switch result {
+            case .success(let resp):
+                print("[I] users: \(String(describing: resp.data))")
+                break
             case .failure(let error):
                 print("[E] error: \(error)")
             }
@@ -40,7 +70,7 @@ class ViewController: UIViewController {
         
         // Gets cookies, and so on.
         CXNetWorkManager.shared.onRequestCompletion = { [weak self] response in
-            print("[I] response: \(response), httpURLRespone: \(response.response)")
+            print("[I] response: \(response), httpURLRespone: \(String(describing: response.response))")
         }
     }
     
